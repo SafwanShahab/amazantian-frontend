45bb1a3472a06bc7542d6b6286495ee63fd6fa59
Safwan Shahab <79778141+SafwanShahab@users.noreply.github.com>
2025-03-07 06:25:52 -0800

    fixed backgroundvid nt working

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App: React.FC = () => {
               <div className="homepage">
                 {/* Background video */}
                 <video autoPlay muted loop className="background-video">
-                  <source src="cityview.mp4" type="video/mp4" />
+                  <source src="Cityview.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
 
